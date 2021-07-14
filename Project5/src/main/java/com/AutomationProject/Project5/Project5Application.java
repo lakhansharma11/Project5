b1be76d29eebba74998298a87dc99d76bb4406c1
lakhansharma081 <lakhansharma081@ip-172-31-67-220.ec2.internal>
2021-07-14 20:48:30 +0000

    final push change package type jar to war

--- Project5/src/main/java/com/AutomationProject/Project5/Project5Application.java
+++ Project5/src/main/java/com/AutomationProject/Project5/Project5Application.java
@@ -2,9 +2,10 @@ package com.AutomationProject.Project5;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class Project5Application {
+public class Project5Application extends SpringBootServletInitializer {
 
 	public static void main(String[] args) {
 		SpringApplication.run(Project5Application.class, args);
--- Project5/src/main/java/com/AutomationProject/Project5/controller/ApplicationController.java
+++ Project5/src/main/java/com/AutomationProject/Project5/controller/ApplicationController.java
@@ -1,13 +1,16 @@
 package com.AutomationProject.Project5.controller;
 
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+
 @RestController
+@RequestMapping("/SBA")
 
 public class ApplicationController {
 	
-	@RequestMapping("/SBA")
+	@GetMapping("/SBA1")
 	public String hello()
 	{
 		return "Welcome to java spring boot appication";
